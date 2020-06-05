e641c885da9b05e10a99b198c26482f8e7169ed0
Sandra Gadomska <gadomska.sandra@gmail.com>
2020-06-05 17:06:31 +0200

    initial commit

---
+++ src/main/java/com/gadomska/myjourneyapp/MyJourneyAppApplication.java
@@ -0,0 +1,13 @@
+package com.gadomska.myjourneyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJourneyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJourneyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gadomska/myjourneyapp/MyJourneyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gadomska.myjourneyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJourneyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
